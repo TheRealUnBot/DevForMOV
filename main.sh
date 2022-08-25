4cb932c4c738c35bc7e56535db0c098d8e6366aa
TheRealUnBot <161210@student.vansd.org>
2022-08-25 16:37:30 +0000

    test

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ echo 2: Compile Client;
 echo 3: Start Server for Testing;
 echo 4: Compile Both and Start Server;
 echo 5: Push All Changes to Github;
+echo 6: Pull All Changes from Github;
 read -p "Answer:" ans;
 case $ans in
   1 ) echo Compiling EPK...; java -jar epkcompiler/CompilePackage.jar "resources" "output/assets.epk"; echo Compiled EPK; exit;;
@@ -11,4 +12,5 @@ case $ans in
   4 ) echo Compiling Client...; sh gradlew teavmc; echo Compiled classes.js; echo Compiling EPK...; java -jar epkcompiler/CompilePackage.jar "resources" "output/assets.epk"; echo Compiled EPK; echo Starting Server for Testing...; python -m http.server --directory output; exit;;
   3 ) echo Starting Server for Testing...; python -m http.server --directory output; exit;;
   5 ) git init; git add -A; git commit -m 'test'; git remote set-url origin https://TheRealUnBot:$KEY_GIT@github.com/TheRealUnBot/DevForMOV.git; git push -u origin master; exit;;
+  6 ) git pull; exit;;
 esac
